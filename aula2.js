13ff2ed0969e111aec0e5b1b56bf71211cdd678c
maximilianosilva753 <maximilianosilva753@gmail.com>
2018-12-14 13:48:42 -0200

    Add file class 2

---
+++ aula2.js
@@ -0,0 +1,33 @@
+//Operadores
+	//'+' adição
+	//'-' subtração
+	//'*' multiplicação
+	//'/' divisão
+
+//Operadores Abreviados:
+	//++ (para abreviar a soma do pós incremento)
+	//--
+
+		var soma2numeros = 1
+		
+		 soma = soma + 1
+		 soma++ //utilizando abreviação ++ irá repetir a soma da linha acima 
+		
+		var sub2num = 10
+		
+			sub2num = sub2num - 1
+			sub2num-- //A mesma regra se aplica na subtração
+
+
+//Outros operadores abreviados
+	//'+=' usando com a mesma lógica de soma = soma + <valor>
+	//'-=' usando com a mesma lógica de soma = soma - <valor>
+	//'*=' usando com a mesma lógica de soma = soma * <valor>
+	//'/=' usando com a mesma lógica de soma = soma / <valor>
+
+
+var somamaisresultado = 1
+	soma += 1
+
+var subcomresultado = 10
+	var -= 1
