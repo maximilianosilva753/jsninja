a5d4223fe7048d208d2ea27b473064e72ec06a74
maximilianosilva753 <maximilianosilva753@gmail.com>
2018-12-14 16:44:58 -0200

    Add file class 3

---
+++ aula3.js
@@ -0,0 +1,51 @@
+//Operadores de igualdade/relacionais
+	//"==" igualdade - Serve para testar se um valor é igual ao outro. Ex: 1 == 1 (o retorno desse comando será true, pois os valores são iguais).
+	//"!=" - Com esse operador ele irá retornar se é verdadeiro que os valores são diferentes. Ex: 1 != 1 (O retorno desse comando será false, pois os valores não são diferentes).
+
+		var x = 0
+
+			x == 0 //(true)
+			x != 0 //(false)
+
+			"max" == "max" //(true)
+
+		var nome = "max"
+
+			nome == "carlos" //(false)
+
+		var pessoa = "maria"
+
+			nome == pessoa //(false)
+
+		var umstring = "1"
+		var umnumero = 1
+
+			umstring == umnumero //(resultado será true, mesmo uma variavel sendo string e outra int, pois o JS é de tipagem fraca e faz a conversão automaticamente.)
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+//Mais operadores igualdade/relacional: Servem para testar também o TIPO;
+
+	//'===' Igual a, e do mesmo tipo: 
+	//'!==' Diferente, mas do mesmo tipo: 
+		//Ex:
+		 1 === '1'  //(retornará false, pois valida que são de valores iguais mas o tipo é diferente.)
+		 1 !== '1'  //(retornará true, pois são de valores iguais mas o tipo é diferente.)
+		 2 === 1    //(retornará false, pois são de tipos iguais mas os valores são diferentes.)
+		"1" !== "1" //(retorna false pois são de tipos e valores iguais.) 
+
+
+//Operadores relacionais:
+	//'>'
+	//'<'
+	//'>='
+	//'<='
+
+
+
+
