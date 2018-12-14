94949a979a6b610e1d06d211d9240d86e856ed38
maximilianosilva753 <maximilianosilva753@gmail.com>
2018-12-14 11:45:45 -0200

    Variable and type

---
+++ aula1.js
@@ -0,0 +1,19 @@
+var nomevariavel = 1
+var nomevariavelstring = "Nome"
+var nomevariavelboolean = true
+var nomevariavelnull = null
+var nomevariavelundefined = undefined
+
+var variravelobject = {}
+
+	variravelobject = {
+		...alt: 1.80,
+		...peso: 90
+		...}
+
+var variavelarray = []
+
+	variavelarray = [1,2,3,4,5,6] 
+	//Para acessar o valor da variavel necessita colocar o valor da posição do array entre []. Lembrando que a primeira posição será 0. Ex: variavelarray[0]
+
+
