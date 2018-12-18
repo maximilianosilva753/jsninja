0b5e825ffdad4095710062fbefdaa25435cd258c
Matheuskbrasil <matheus.070202@gmail.com>
2018-12-18 09:31:02 -0200

    Add lesson 17

---
+++ lesson/aula17.js
@@ -0,0 +1,23 @@
+
+//Criar objeto pessoa:
+var pessoa = {nome: "Max", idade: 30, altura: 1.73, peso: 80};
+
+	//Para acessar o valor da propriedade idade do objeto pessoa, digite o valor abaixo:
+		pessoa.idade;
+
+	//Para editar um propriedade já existente no objeto, precisa realizar o comando abaixo:
+		pessoa.peso = 80;
+
+	//Metodos são ações criadas nas propriedades do objeto, e uma forma de criar um metodo é incluir uma fũnção nas propriedades do objeto:
+		pessoa.aniversario = function() {
+			pessoa.idade++;
+		}
+
+		pessoa.aniversario();
+
+		pessoa.nomeCompleto = function(){
+			return pessoa.nome + " " + pessoa.sobrenome;
+		}
+
+		pessoa.nomeCompleto();
+
